d4b5323484801be668411af894ccab0f1e3a2f72
srinivas kumar <seenukumar124@gmail.com>
2022-10-25 09:50:14 +0000

    React JS coding practice 1

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="card-container">
+    <h1 className="card-heading">Congractulations</h1>
+    <div className="profile-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="profile-image"
+      />
+      <h1 className="profile-name">Kiran V</h1>
+      <p className="profile-description">
+        Vishnu Institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="watch-image"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
